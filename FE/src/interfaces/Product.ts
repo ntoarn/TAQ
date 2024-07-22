59beb3d1757052ac2aec0bb568401a03a8e781b6
ntoarn <nqton301004@gmail.com>
2024-07-22 14:29:55 +0700

    ok

--- FE/src/interfaces/Product.ts
+++ FE/src/interfaces/Product.ts
@@ -4,11 +4,20 @@ export interface Product {
     price: number;
     description: string;
     image: string;
-    color?: string;
+    colorId?: string;
+    sizeId?: string;
     categoryId?: Category;
 }
 export interface Category{
     _id?: string;
     name:string;
     slug:string
+}
+export interface Color{
+    _id?: string;
+    color:string;
+}
+export interface Size{
+    _id?: string;
+    size:string;
 }
